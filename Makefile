5579ce08a65b4ef74c1ad07c0ca16b026ce39b64
Muh Muhten <muh.muhten@gmail.com>
2016-09-09 00:12:41 -0400

    include jail_inspect, a jail syscall debug tool
    Sample usage:
    LD_PRELOAD=./jail_inspect.so jail -c ip4=inherit command=false

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 gaogao:	gaogao.c
-	cc -pipe -Wall -pedantic -Os -o $@ $<
+	cc -pipe -Wall -pedantic -Os -o $@ $>
+
+jail_inspect.so:	jail_inspect.c
+	cc -pipe -Wall -pedantic -Os -shared -fPIC -o $@ $>
---
+++ jail_inspect.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <sys/param.h>
+#include <sys/jail.h>
+#include <errno.h>
+#include <sys/uio.h>
+#include <dlfcn.h>
+#include <string.h>
+#include <stdlib.h>
+
+int jail(struct jail *jail) {
+  int (*old)(struct jail *) = (int (*)(struct jail *))dlsym(RTLD_NEXT, "jail");
+  int res = old(jail);
+  printf("jail(%p) = %d\n", jail, res);
+  if (res == -1) printf("%d %s\n", errno, strerror(errno));
+  return res;
+}
+
+int jail_get(struct iovec *iov, u_int niov, int flags) {
+  int (*old)(struct iovec *, u_int, int) =
+    (int (*)(struct iovec *, u_int, int))dlsym(RTLD_NEXT, "jail_get");
+  struct iovec *oiov = iov;
+  u_int oniov = niov;
+  printf("iovec * %p\n", iov);
+  while (niov--) {
+    char **base = iov->iov_base;
+    int len = (int)iov->iov_len;
+    printf("\t%d %d %*s\n", len, base ? (int)*base : 0, -len, (char *)base);
+    iov++;
+  }
+  int res = old(oiov, oniov, flags);
+  printf("jail_get(%p, %u, %d) = %d\n", iov, niov, flags, res);
+  if (res == -1) printf("%d %s\n", errno, strerror(errno));
+  return res;
+}
+
+int jail_set(struct iovec *iov, u_int niov, int flags) {
+  unsetenv("LD_PRELOAD");
+  int (*old)(struct iovec *, u_int, int) =
+    (int (*)(struct iovec *, u_int, int))dlsym(RTLD_NEXT, "jail_set");
+  struct iovec *oiov = iov;
+  u_int oniov = niov;
+  int res = old(oiov, oniov, flags);
+  printf("iovec * %p\n", iov);
+  while (niov--) {
+    char **base = iov->iov_base;
+    int len = (int)iov->iov_len;
+    printf("\t%d %d %*s\n", len, base ? (int)*base : 0, -len, (char *)base);
+    iov++;
+  }
+  printf("jail_set(%p, %u, %d) = %d\n", iov, niov, flags, res);
+  if (res == -1) printf("%d %s\n", errno, strerror(errno));
+  return res;
+}
+
+int jail_attach(int jid) {
+  int (*old)(int) = (int (*)(int))dlsym(RTLD_NEXT, "jail_attach");
+  int res = old(jid);
+  printf("jail_attach(%d) = %d\n", jid, res);
+  if (res == -1) printf("%d %s\n", errno, strerror(errno));
+  return res;
+}
+
+int jail_remove(int jid) {
+  int (*old)(int) = (int (*)(int))dlsym(RTLD_NEXT, "jail_remove");
+  int res = old(jid);
+  printf("jail_remove(%d) = %d\n", jid, res);
+  if (res == -1) printf("%d %s\n", errno, strerror(errno));
+  return res;
+}
